3c3c0c1f0b0844493d50747406f9fb6cda017127
Shane Ingwersen <david.ingwersen@va.gov>
2021-05-17 13:57:16 -0500

    add bin/bash

--- build/create-tasks-and-run-pipeline.sh
+++ build/create-tasks-and-run-pipeline.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "Create tasks..."
 oc create --save-config=true -f image-streams/imageStream-shane-rest-app.yaml 
 oc create --save-config=true -f persistentVolumes/task-cache-pvc.yaml 
